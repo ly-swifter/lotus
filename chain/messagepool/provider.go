b6b3fc72bc2ace2ea9038b6379552522f4ed1718
Phi <orjan.roren@gmail.com>
2023-09-04 15:34:15 +0200

    Revert "Capture profile if GetActorAfter exceeds 2 seconds"
    This reverts commit ebe239dc9893a79e0fe5e237e308383a82db15e8.

--- chain/messagepool/provider.go
+++ chain/messagepool/provider.go
@@ -3,10 +3,6 @@ package messagepool
 import (
 	"context"
 	"errors"
-	"fmt"
-	"os"
-	"runtime/pprof"
-	"sync"
 	"time"
 
 	"github.com/ipfs/go-cid"
@@ -26,8 +22,6 @@ var (
 	HeadChangeCoalesceMinDelay      = 2 * time.Second
 	HeadChangeCoalesceMaxDelay      = 6 * time.Second
 	HeadChangeCoalesceMergeInterval = time.Second
-	profileCounter                  int
-	profileMutex                    sync.Mutex
 )
 
 type Provider interface {
@@ -111,20 +105,6 @@ func (mpp *mpoolProvider) GetActorBefore(addr address.Address, ts *types.TipSet)
 }
 
 func (mpp *mpoolProvider) GetActorAfter(addr address.Address, ts *types.TipSet) (*types.Actor, error) {
-	profileMutex.Lock()
-	profileCounter++
-	f, err := os.Create(fmt.Sprintf("cpu_%d.pprof", profileCounter))
-	profileMutex.Unlock()
-	if err != nil {
-		log.Fatal("could not create CPU profile: ", err)
-	}
-	if err := pprof.StartCPUProfile(f); err != nil {
-		log.Fatal("could not start CPU profile: ", err)
-	}
-	defer pprof.StopCPUProfile()
-
-	start := time.Now()
-
 	if mpp.IsLite() {
 		return mpp.getActorLite(addr, ts)
 	}
@@ -137,13 +117,6 @@ func (mpp *mpoolProvider) GetActorAfter(addr address.Address, ts *types.TipSet) 
 	if err != nil {
 		return nil, xerrors.Errorf("failed to load state tree: %w", err)
 	}
-
-	elapsed := time.Since(start)
-	if elapsed <= 2*time.Second {
-		// If the execution time didn't exceed 2 seconds, remove the profile.
-		os.Remove(fmt.Sprintf("cpu_%d.pprof", profileCounter))
-	}
-
 	return st.GetActor(addr)
 }
 
